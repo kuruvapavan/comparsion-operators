04485c0d6f2e748c046099f75ff44693f5e932f1
kuruvapavan <94837551+kuruvapavan@users.noreply.github.com>
2021-12-14 19:51:08 +0530

    updated

---
+++ comparsionopeartors.js
@@ -0,0 +1,45 @@
+if (10==10){
+console.log(true)
+}else {
+    console.log(false)
+}
+
+if (10==9){
+    console.log(true)
+    }else {
+        console.log(false)
+    }
+    if (10==='10'){
+        console.log(true)
+        }else {
+            console.log(false)
+        }
+        if (10===9){
+            console.log(true)
+            }else {
+                console.log(false)
+            }
+            if (10!=9){
+                console.log(true)
+                }else {
+                    console.log(false)
+                }
+
+                if (10!=10){
+                    console.log(true)
+                    }else {
+                        console.log(false)
+                    }
+                    if (10!=='10'){
+                        console.log(true)
+                        }else {
+                            console.log(false)
+                        }
+
+
+                        if (10!=='9'){
+                            console.log(true)
+                            }else {
+                                console.log(false)
+                            }
+                            
